f9bc880912590c58feff23816b62cca3da4d9157
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-08 15:41:34 +0200

    MovieController collegato con il db

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     //
-    public function Index(){
-        return view('home');
+    public function index(){
+        $movies = Movie::all();
+        dd($movies);
+        return view('home', compact('movies'));
     }
 }
